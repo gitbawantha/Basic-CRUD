a802cdaedc21f87544cb032b2aca7f4ff05e95d6
Bawantha Prasad Bandara <bawantha.bandara@iits.biz>
2023-02-21 11:56:11 +0530

    Delete Option

--- TheShopper/Controllers/Category/CategoryController.cs
+++ TheShopper/Controllers/Category/CategoryController.cs
@@ -81,5 +81,36 @@ namespace TheShopper.Controllers
             }
             return View(obj);
         }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)          
+        {
+            var obj = _db.Categories.Find (id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");           
+        }
     }
 }
